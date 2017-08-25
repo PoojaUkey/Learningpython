de0ad51d69cd4fb3cc5b08ee3161bb9899c19cb4
PoojaUkey <poo.ukey@gmail.com>
2017-08-25 15:26:54 +0530

    Pattern1.py

---
+++ Pattern1.py
@@ -0,0 +1,9 @@
+def pattern(num):
+
+    for i in range(1,num+1):
+        for j in range(1,i+1):
+            print '*',
+        print
+if __name__ == '__main__':
+    num = input("Enter number of rows")
+    r=pattern(num)
